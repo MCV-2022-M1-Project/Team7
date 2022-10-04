95ba359574b656cfcc0f060e358d7dc8f7dc71ce
AtenrevCode <atenrev@gmail.com>
2022-10-04 12:24:57 +0200

    Added an example of FeaturesExtractor

---
+++ src/extractors/example.py
@@ -0,0 +1,16 @@
+import array as np
+import numpy as np
+from typing import Dict
+
+from extractors.base import FeaturesExtractor
+
+
+class ExampleExtractor(FeaturesExtractor):
+    name: str = "example_extractor"
+
+    def run(features: np.ndarray) -> Dict[str, np.ndarray]:
+        #do stuff
+        return {
+            "output": np.array([1,2,3,4]),
+            # Extra results if needed...
+        }
