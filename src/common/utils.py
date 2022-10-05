b5b4dcfd07621b35ae08ebc33ada32635a3893a0
AYAN BANERJEE <48727074+ayanban011@users.noreply.github.com>
2022-10-05 12:29:14 +0200

    Update utils.py

--- src/common/utils.py
+++ src/common/utils.py
@@ -43,3 +43,46 @@ def image_normalize(img):
         HxW [0,255]
     """
     return img/(np.amax(img)+1e-7)
+
+def compute_pre_rec(gt,mask,mybins=np.arange(0,256)):
+    """
+        Auxiliary function for computing precision and recall
+        
+        Args:
+            gt: ground-truth mask
+            mask: predicted mask
+        output:
+            confusion matrix
+    """
+
+    if(len(gt.shape)<2 or len(mask.shape)<2):
+        print("ERROR: gt or mask is not matrix!")
+        exit()
+    if(len(gt.shape)>2): # convert to one channel
+        gt = gt[:,:,0]
+    if(len(mask.shape)>2): # convert to one channel
+        mask = mask[:,:,0]
+    if(gt.shape!=mask.shape):
+        print("ERROR: The shapes of gt and mask are different!")
+        exit()
+
+    gtNum = gt[gt>128].size # pixel number of ground truth foreground regions
+    pp = mask[gt>128] # mask predicted pixel values in the ground truth foreground region
+    nn = mask[gt<=128] # mask predicted pixel values in the ground truth bacground region
+
+    pp_hist,pp_edges = np.histogram(pp,bins=mybins) #count pixel numbers with values in each interval [0,1),[1,2),...,[mybins[i],mybins[i+1]),...,[254,255)
+    nn_hist,nn_edges = np.histogram(nn,bins=mybins)
+
+    pp_hist_flip = np.flipud(pp_hist) # reverse the histogram to the following order: (255,254],...,(mybins[i+1],mybins[i]],...,(2,1],(1,0]
+    nn_hist_flip = np.flipud(nn_hist)
+
+    pp_hist_flip_cum = np.cumsum(pp_hist_flip) # accumulate the pixel number in intervals: (255,254],(255,253],...,(255,mybins[i]],...,(255,0]
+    nn_hist_flip_cum = np.cumsum(nn_hist_flip)
+
+    precision = pp_hist_flip_cum/(pp_hist_flip_cum + nn_hist_flip_cum+1e-8) #TP/(TP+FP)
+    recall = pp_hist_flip_cum/(gtNum+1e-8) #TP/(TP+FN)
+
+    precision[np.isnan(precision)]= 0.0
+    recall[np.isnan(recall)] = 0.0
+
+    return np.reshape(precision,(len(precision))),np.reshape(recall,(len(recall)))
