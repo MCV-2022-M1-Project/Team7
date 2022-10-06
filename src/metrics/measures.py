Fixed masking metrics

src/metrics/measures.py
@@ -13,27 +13,24 @@ from src.common.utils import binarize, image_normalize, compute_pre_rec
 class MAE(Metric):
     name: str = "mae"
     
-    def compute(self,mask1,mask2):
+    def compute(self,ground_truth,predictions):
         """
             Compute the mean absolute error
             
             Args:
-                'mask1': HxW or HxWxn (asumme that all the n channels are the same and only the first channel will be used)
-                'mask2': HxW or HxWxn
+                'ground_truth': HxW or HxWxn (asumme that all the n channels are the same and only the first channel will be used)
+                'predictions': HxW or HxWxn
             
             Returns: 
                 a value MAE, Mean Absolute Error
         """
-        mask1 = array(mask1)
-        mask2 = array(mask2)
-        mask1 = image_normalize(mask1)
-        mask2 = image_normalize(mask2)
-        h,w = mask1.shape[0],mask1.shape[1]
-        mask1 = image_normalize(mask1)
-        mask2 = image_normalize(mask2)
-        sumError = np.sum(np.absolute((mask1.astype(float) - mask2.astype(float))))
+        ground_truth = array(ground_truth)
+        predictions = array(predictions)
+        ground_truth = image_normalize(ground_truth)
+        predictions = image_normalize(predictions)
+        h, w = ground_truth.shape[-2],ground_truth.shape[-1]
+        sumError = np.sum(np.absolute((ground_truth.astype(float) - predictions.astype(float))))
         maeError = sumError/((float(h)*float(w)+1e-8))
-
         return maeError
     
 
@@ -41,49 +38,44 @@ class MAE(Metric):
 class Precision(Metric):
     name: str = "precision"
     
-    def compute(self,mask1,mask2):
+    def compute(self,ground_truth,predictions):
         """
             Compute the precision
             
             Args:
-                'mask1': ground truth
-                'mask2': predictions
+                'ground_truth': ground truth
+                'predictions': predictions
             
             Returns: 
                  precision
         """
-        mask1 = array(mask1)
-        mask2 = array(mask2)
-        mask1 = binarize(mask1)
-        mask2 = binarize(mask2)
-        #true_values = mask1
-        #predictions = mask2
-        #print(true_values)
-        #print(predictions)
-        #N = true_values.shape[1]
-        precision = (mask1==mask2).sum()/(mask2.sum()+1e-8)
+        ground_truth = array(ground_truth)
+        predictions = array(predictions)
+        ground_truth = binarize(ground_truth)
+        predictions = binarize(predictions)
+        precision = ((predictions == 1) & (ground_truth == 1)).sum()/(predictions.sum()+1e-8)
         return precision
     
 @Registry.register_metric    
 class Recall(Metric):
     name: str="recall"
     
-    def compute(self,mask1,mask2):
+    def compute(self,ground_truth,predictions):
         """
             Compute the precision
             
             Args:
-                'mask1': ground truth
-                'mask2': predictions
+                'ground_truth': ground truth
+                'predictions': predictions
             
             Returns: 
                  recall
         """
-        mask1 = array(mask1)
-        mask2 = array(mask2)
-        mask1 = binarize(mask1)
-        mask2 = binarize(mask2)
-        recall = (mask1==mask2).sum()/(mask1.sum()+1e-8)
+        ground_truth = array(ground_truth)
+        predictions = array(predictions)
+        ground_truth = binarize(ground_truth)
+        predictions = binarize(predictions)
+        recall = ((predictions == 1) & (ground_truth == 1)).sum()/(ground_truth.sum()+1e-8)
         return recall
 
 
@@ -91,22 +83,22 @@ class Recall(Metric):
 class F1(Metric):
     name: str="f1"
     
-    def compute(self,mask1,mask2):
+    def compute(self,ground_truth,predictions):
         """
             Compute the f1
             
             Args:
-                'mask1': ground truth
-                'mask2': predictions
+                'ground_truth': ground truth
+                'predictions': predictions
             
             Returns: 
                  f1
         """
 
-        mask1 = array(mask1)
-        mask2 = array(mask2)
-        mask1 = binarize(mask1)
-        mask2 = binarize(mask2)
-        precision = (mask1==mask2).sum()/(mask2.sum()+1e-8)
-        recall = (mask1==mask2).sum()/(mask1.sum()+1e-8)
+        ground_truth = array(ground_truth)
+        predictions = array(predictions)
+        ground_truth = binarize(ground_truth)
+        predictions = binarize(predictions)
+        precision = ((predictions == 1) & (ground_truth == 1)).sum()/(predictions.sum()+1e-8)
+        recall = ((predictions == 1) & (ground_truth == 1)).sum()/(ground_truth.sum()+1e-8)
         return 2*recall*precision/(recall+precision)