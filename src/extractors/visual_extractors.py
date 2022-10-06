97281b98e7b842c2d8c51b563cca7993bb11cb32
EauDeData <evabot505@gmail.com>
2022-10-06 15:55:49 +0200

    Bugfix

--- src/extractors/visual_extractors.py
+++ src/extractors/visual_extractors.py
@@ -12,7 +12,7 @@ from src.extractors.base import FeaturesExtractor
 
 
 # Vsual codebook
-def get_codebooks(sample_hz: int = 256, max_exp: int = 25, norm_max: int = 1):
+def get_codebooks(sample_hz: int = 255, max_exp: int = 25, norm_max: int = 1):
     
     norm_signal = lambda x: norm_max * ((x - x.min())/(x.max() - x.min()))
     
@@ -63,11 +63,6 @@ def tohsv(img):
 @Registry.register_features_extractor
 class VisualCodebookExtractor(FeaturesExtractor):
     name: str = "visual_codebook_extractor"
-    
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.codebook: List[np.array]= get_codebooks()
 
     def run(self, images: List[np.ndarray], k_size: int = 25, threshold_as = 0.5, channel: int = 0) -> Dict[str, np.ndarray]:
         """
@@ -84,7 +79,7 @@ class VisualCodebookExtractor(FeaturesExtractor):
             A dictionary whose result key is the list of computed histograms.
         """
 
-        ## Process as HSV
+        self.codebook: List[np.array]= get_codebooks()
 
 
         cosine_similarity = lambda x, y: 1 - spatial.distance.cosine(x, y) # Cosine similarity is performed instead of euclidean.
@@ -96,7 +91,7 @@ class VisualCodebookExtractor(FeaturesExtractor):
             local_codebook = codebook_hist_bins.copy()
             for i_step in range(0, img.shape[0], k_size):
                 for j_step in range(0, img.shape[1], k_size):
-                    hist, _ = np.histogram(img[i_step:i_step+k_size, j_step:j_step+k_size, channel], 256)
+                    hist, _ = np.histogram(img[i_step:i_step+k_size, j_step:j_step+k_size, channel], 255)
                     scores = [cosine_similarity(hist, x) for x in self.codebook]
                     max_score = np.argmax(scores)
                     if scores[max_score] >= threshold_as: local_codebook[max_score] += 1
