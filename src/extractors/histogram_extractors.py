2fde1ba1a0ac613ad41b86b98a8d4286ff0b4a1c
AtenrevCode <atenrev@gmail.com>
2022-10-09 13:55:51 +0200

    small detail

--- src/extractors/histogram_extractors.py
+++ src/extractors/histogram_extractors.py
@@ -53,8 +53,7 @@ class HistogramRGBConcatExtractor(FeaturesExtractor):
 
         for image in images:
             bgr_planes = cv2.split(image)
-            # image_feats = np.concatenate([cv2.calcHist(bgr_planes, [i], None, [256], [0, 256]).ravel() for i in range(3)])
-            image_feats = cv2.calcHist(bgr_planes, [0], None, [256], [0, 256]).ravel() 
+            image_feats = np.concatenate([cv2.calcHist(bgr_planes, [i], None, [256], [0, 256]).ravel() for i in range(3)])
             image_feats = image_feats / np.sum(image_feats)
             image_feats_list.append(image_feats)
 
