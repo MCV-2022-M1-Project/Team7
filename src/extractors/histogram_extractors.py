1ecffdb792abe4c22d320cd19e221de2c8ec5dd3
EauDeData <evabot505@gmail.com>
2022-10-11 11:35:17 +0200

    Changes

--- src/extractors/histogram_extractors.py
+++ src/extractors/histogram_extractors.py
@@ -231,7 +231,7 @@ class PyramidLocalHistogramExtractor(FeaturesExtractor):
 
         return image_feature
 
-    def run(self, images: List[np.ndarray], initial_patches: int = 2, num_pyramid_levels: int = 6, channel: int = 0, sample: int = 48, **kwargs) -> Dict[str, np.ndarray]:
+    def run(self, images: List[np.ndarray], initial_patches: int = 2, num_pyramid_levels: int = 6, channel: int = 0, sample: int = 48, *args, **kwargs) -> Dict[str, np.ndarray]:
 
         features = []
 
