28c1b2747753c271eb911d646bea3715cd2ff090
AtenrevCode <atenrev@gmail.com>
2022-11-04 17:57:02 +0100

    task adapted to SIFT (unfinished)

--- src/extractors/visual_extractors.py
+++ src/extractors/visual_extractors.py
@@ -1,16 +1,10 @@
 import array as np
-from threading import local
 import cv2
 import numpy as np
 import scipy.stats as stats
+import concurrent
 from typing import Dict, List
-from scipy import spatial
-from sklearn.metrics import euclidean_distances
-from sympy import euler
-from sklearn.cluster import KMeans
-import multiprocessing as mp
 
-from src.common.utils import image_normalize
 from src.common.registry import Registry
 from src.extractors.base import FeaturesExtractor
 from src.tokenizers.base import BaseTokenizer
@@ -19,60 +13,53 @@ from src.tokenizers.base import BaseTokenizer
 # Vsual codebook
 def get_artificial_codebook(sample_hz: int = 255, max_exp: int = 25, norm_max: int = 1):
 
-    ### WHAT IF CODEBOOKS ARE RANDOM NOISE COMPOSITION OF SIGNALS????
-    
-    norm_signal = lambda x: norm_max * ((x - x.min())/(x.max() - x.min()))
-    
-    ### Normal
+    # WHAT IF CODEBOOKS ARE RANDOM NOISE COMPOSITION OF SIGNALS????
+
+    def norm_signal(x): return norm_max * ((x - x.min())/(x.max() - x.min()))
+
+    # Normal
     mu = 0
     variance = 1
     sigma = variance**.5
-    
+
     xnormal = np.linspace(mu - 3*sigma, mu + 3*sigma, sample_hz)
-    y_normal = stats.norm.pdf(xnormal, mu, sigma)    
-    
-    ### Normal Inverse
+    y_normal = stats.norm.pdf(xnormal, mu, sigma)
+
+    # Normal Inverse
     y_normal_inverse = -y_normal
-    
-    
+
     ### Exponential - Left
     y_exp = np.linspace(0, max_exp, sample_hz)
     y_exp_left = stats.expon.pdf(y_exp)
 
-    
-    
-    ### Exponential Inverse - Left
+    # Exponential Inverse - Left
     y_exp_inv_left = - y_exp_left
-    
-    
+
     ### Exponential - Right
     y_exp_right = y_exp_inv_left[::-1]
-    
-    
-    ### Exponential Inverse - Right
+
+    # Exponential Inverse - Right
     y_exp_inv_right = - y_exp_right
-    
-    
-    
-    ### Constant
+
+    # Constant
     y_constant = np.ones(sample_hz)
-    
-    
-    ### Normalize Distributions y: (0-1)
+
+    # Normalize Distributions y: (0-1)
     distributions = [y_normal, y_normal_inverse,
-                    y_exp_left, y_exp_inv_left,
-                    y_exp_right, y_exp_inv_right,]
+                     y_exp_left, y_exp_inv_left,
+                     y_exp_right, y_exp_inv_right, ]
     return [norm_signal(x) for x in distributions] + [y_constant]
+
 
 def tohsv(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
 
 @Registry.register_features_extractor
 class VisualCodebookExtractor(FeaturesExtractor):
     name: str = "visual_codebook_extractor"
 
     def run(self, images: List[np.ndarray], tokenizer: BaseTokenizer = None) -> Dict[str, np.ndarray]:
-
         """
         Extractor that process the histogram of occurences of a certain visual word with respect a fixed codebook.
         The histogram of codebook appearences acts as feature for the image. Given a certain path, its codebook is the one with maximum cosine similarity.
@@ -86,11 +73,13 @@ class VisualCodebookExtractor(FeaturesExtractor):
         Returns:
             A dictionary whose result key is the list of computed histograms.
         """
-        assert isinstance(tokenizer, BaseTokenizer), f'Visual Codebook Extractor must receive a tokenizer as a parameter. Received: {tokenizer}'
+        assert isinstance(
+            tokenizer, BaseTokenizer), f'Visual Codebook Extractor must receive a tokenizer as a parameter. Received: {tokenizer}'
         features = tokenizer.tokenize(images)["result"]
         return {
-            "result": features 
+            "result": features
         }
+
 
 @Registry.register_features_extractor
 class RandomFeaturesExtractor(FeaturesExtractor):
@@ -109,47 +98,63 @@ class RandomFeaturesExtractor(FeaturesExtractor):
             A dictionary whose result key is the list of computed histograms.
         """
 
-
-
         return {
             "result": np.random.rand(len(images), 10),
         }
-    
+
 
 @Registry.register_features_extractor
 class SIFTExtractor(FeaturesExtractor):
     name: str = "sift_features_extractor"
-    def __init__(self, n_keypoints = 3, n_threads = 4, *args, **kwargs) -> None:
+
+    def __init__(self, n_keypoints=40, n_threads=4, *args, **kwargs) -> None:
         self.n_keypoints = n_keypoints
         self.sift_size = 128
         self.descriptor_size = n_keypoints * self.sift_size
         self.n_threads = n_threads
-    
-    def process_imgs_mp(self, images, sift, features, k):
-        for n in range(k, len(images), self.n_threads):
 
-            image = images[n]
-            keypoints, descriptors = sift.detectAndCompute(image, None)
-
-            if len(keypoints) > self.n_keypoints: descriptors = descriptors[:self.n_keypoints, :]
-            left = self.n_keypoints if not isinstance(descriptors, np.ndarray) else self.n_keypoints - len(descriptors)
-            if left:
-                fill = np.zeros(left *self.sift_size )
-                if isinstance(descriptors, np.ndarray): fill = np.hstack((descriptors.reshape(-1), fill))
-                features[n] = fill
-            else: features[n]= descriptors.reshape(-1)
-            if len(features[n]) != self.descriptor_size: print(descriptors)
-
-    def run(self, images: List[np.ndarray], **kwargs) -> Dict[str, np.ndarray]:
+    def process_imgs_mp(self, data):
+        image = data[0]
         sift = cv2.SIFT_create(self.n_keypoints)
 
-        mp_manager = mp.Manager()
-        features = mp_manager.list([None for i in range(len(images))])
+        keypoints, descriptors = sift.detectAndCompute(image, None)
 
-        threads = [mp.Process(target=self.process_imgs_mp, args=(images, sift, features, i)) for i in range(self.n_threads)]
-        [t.start() for t in threads]
-        [t.join() for t in threads]
+        if not isinstance(descriptors, np.ndarray):
+            descriptors = np.zeros((1, self.sift_size), dtype=np.float32)
+
+        return descriptors
+
+        # if len(keypoints) > self.n_keypoints:
+        #     descriptors = descriptors[:self.n_keypoints, :]
+        # left = self.n_keypoints if not isinstance(
+        #     descriptors, np.ndarray) else self.n_keypoints - len(descriptors)
+
+        # if left:
+        #     fill = np.zeros(left * self.sift_size, dtype=np.float32)
+        #     if isinstance(descriptors, np.ndarray):
+        #         fill = np.hstack((descriptors.reshape(-1), fill))
+        #     return fill
+        # else:
+        #     return descriptors.reshape(-1).astype(np.float32)
+                
+        # if len(features[n]) != self.descriptor_size:
+        #     print(descriptors)
+
+    def run(self, images: List[np.ndarray], **kwargs) -> Dict[str, np.ndarray]:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.n_threads) as executor:
+            features = list(executor.map(
+                self.process_imgs_mp, 
+                [(img, ) for img in images]
+                ))
+
+        # mp_manager = mp.Manager()
+        # features = mp_manager.list([None for i in range(len(images))])
+        
+        # threads = [mp.Process(target=self.process_imgs_mp, args=(
+        #     images, features, i)) for i in range(self.n_threads)]
+        # [t.start() for t in threads]
+        # [t.join() for t in threads]
 
         return {
-            "result": list(features),
+            "result": features,
         }
--- src/tasks/retrieval_distcomb.py
+++ src/tasks/retrieval_distcomb.py
@@ -1,8 +1,9 @@
 import logging
 import os
-from typing import Dict
 import numpy as np
 import pickle
+import cv2
+from typing import Dict
 from tqdm import tqdm
 from sklearn.neighbors import NearestNeighbors
 
@@ -27,29 +28,40 @@ class RetrievalDistCombTask(BaseTask):
             if self.tokenizer.input_type == "image":
                 self.tokenizer.fit(self.query_dataset.images)
             elif self.tokenizer.input_type == "str":
-                self.tokenizer.fit([", ".join(l) for ann in self.retrieval_dataset.annotations for l in ann])
+                self.tokenizer.fit(
+                    [", ".join(l) for ann in self.retrieval_dataset.annotations for l in ann])
 
         logging.info("Extracting retrieval dataset features...")
 
         knn_models: Dict[str, NearestNeighbors] = {}
-        
+
         if self.tokenizer is not None and self.tokenizer.input_type == "str":
-            feats_text = self.tokenizer.tokenize([" ".join(l) if type(l) is list else l for ann in self.retrieval_dataset.annotations for l in ann])
-            distance = Registry.get_distance_instance(self.config.tokenizer.distance)
-            knn_models[self.tokenizer.name] = NearestNeighbors(n_neighbors=self.retrieval_dataset.size, metric=distance.get_reference())
+            feats_text = self.tokenizer.tokenize([" ".join(l) if type(
+                l) is list else l for ann in self.retrieval_dataset.annotations for l in ann])
+            distance = Registry.get_distance_instance(
+                self.config.tokenizer.distance)
+            knn_models[self.tokenizer.name] = NearestNeighbors(
+                n_neighbors=self.retrieval_dataset.size, metric=distance.get_reference())
             knn_models[self.tokenizer.name].fit(feats_text)
 
         if self.extractors is not None:
             for extractor in self.extractors:
-                feats = extractor.run(self.retrieval_dataset.images, tokenizer=self.tokenizer)["result"]
-                distance_config = [e["distance"] for e in self.config.features_extractors if e.name == extractor.name][0]
-                distance = Registry.get_distance_instance(distance_config)
-                knn_models[extractor.name] = NearestNeighbors(n_neighbors=self.retrieval_dataset.size, metric=distance.get_reference())
-                knn_models[extractor.name].fit(feats)
-        
-        final_output_w1=[]
-        final_output_w2=[]
-        
+                feats = extractor.run(
+                    self.retrieval_dataset.images, tokenizer=self.tokenizer)["result"]
+
+                if extractor.name != "sift_features_extractor":
+                    distance_config = [
+                        e["distance"] for e in self.config.features_extractors if e.name == extractor.name][0]
+                    distance = Registry.get_distance_instance(distance_config)
+                    knn_models[extractor.name] = NearestNeighbors(
+                        n_neighbors=self.retrieval_dataset.size, metric=distance.get_reference())
+                    knn_models[extractor.name].fit(feats)
+                else:
+                    sift_features = feats
+
+        final_output_w1 = []
+        final_output_w2 = []
+
         logging.info("Carrying out the task...")
 
         for sample in tqdm(self.query_dataset):
@@ -88,7 +100,8 @@ class RetrievalDistCombTask(BaseTask):
                     for out in output:
                         img = out["result"]
                         text_mask_pred = out["text_mask"]
-                        images_list.append((img * (1-(text_mask_pred[:,:,None] / 255))).astype(np.uint8))
+                        images_list.append(
+                            (img * (1-(text_mask_pred[:, :, None] / 255))).astype(np.uint8))
 
                     if len(images_list) > 0:
                         image = images_list
@@ -98,7 +111,8 @@ class RetrievalDistCombTask(BaseTask):
                         text_transcription.append(out["text"])
 
                         if self.tokenizer is not None and self.tokenizer.input_type == "str":
-                            text_tokens.append(self.tokenizer.tokenize(out["text"])[0])
+                            text_tokens.append(
+                                self.tokenizer.tokenize(out["text"])[0])
 
             if type(image) is not list:
                 image = [image]
@@ -106,22 +120,64 @@ class RetrievalDistCombTask(BaseTask):
             rankings = []
 
             if self.tokenizer is not None and self.tokenizer.input_type == "str":
-                assert len(text_transcription) > 0, "If you use a tokenizer you must set a text detection preprocessor!"
+                assert len(
+                    text_transcription) > 0, "If you use a tokenizer you must set a text detection preprocessor!"
                 feats = np.array(text_tokens)
-                neighs = knn_models[self.tokenizer.name].kneighbors(feats, return_distance=False)
+                neighs = knn_models[self.tokenizer.name].kneighbors(
+                    feats, return_distance=False)
                 ranking = np.array([n.argsort() for n in neighs])
 
                 if self.config.tokenizer.feats_w is None:
-                     self.config.tokenizer.feats_w = 1.0
+                    self.config.tokenizer.feats_w = 1.0
 
                 rankings.append(ranking * self.config.tokenizer.feats_w)
 
             if self.extractors is not None:
                 for extractor in self.extractors:
-                    feats = extractor.run(image, tokenizer=self.tokenizer)["result"]
-                    neighs = knn_models[extractor.name].kneighbors(feats, return_distance=False)
-                    ranking = np.array([n.argsort() for n in neighs])
-                    w = [e["feats_w"] for e in self.config.features_extractors if e.name == extractor.name][0]
+                    feats = extractor.run(
+                        image, tokenizer=self.tokenizer)["result"]
+
+                    if extractor.name != "sift_features_extractor":
+                        neighs = knn_models[extractor.name].kneighbors(
+                            feats, return_distance=False)
+                        ranking = np.array([n.argsort() for n in neighs])
+                    else:
+                        # index_params = dict(algorithm=1, trees=5)
+                        # search_params = dict(checks=50)
+                        # flann = cv2.FlannBasedMatcher(
+                        #     index_params, search_params)
+                        ranking = []
+
+                        for query_feat in feats:
+                            n_matches_per_sample = []
+
+                            for retr_feat in sift_features:
+                                bf = cv2.BFMatcher()
+                                matches = bf.knnMatch(query_feat, retr_feat, k=2)
+
+                                if len(matches[0]) == 1:
+                                    n_matches_per_sample.append(0)
+                                    continue
+
+                                # for m, n in flann.knnMatch(query_feat, retr_feat, k=2):
+                                #     if m.distance < 0.7*n.distance:
+                                #         matches.append(m)
+
+                                n_matches = len([m for m, n in matches if m.distance < 0.7*n.distance])
+                                n_matches_per_sample.append(n_matches)
+                            
+                            ranking.append(np.array(n_matches_per_sample).argsort()[::-1].argsort())
+
+                        ranking = np.array(ranking)
+
+                        # ranking = np.array([
+                        #     np.array([len([m for m, n in flann.knnMatch(query_feat, retr_feat, k=2) if m.distance < 0.7*n.distance])
+                        #               for retr_feat in sift_features]).argsort()[::-1].argsort()
+                        #     for query_feat in feats
+                        # ])
+
+                    w = [e["feats_w"]
+                         for e in self.config.features_extractors if e.name == extractor.name][0]
 
                     if w is None:
                         w = 1.0
@@ -140,7 +196,8 @@ class RetrievalDistCombTask(BaseTask):
             top_k_pred = [np.argsort(r) for r in per_image_rankings]
 
             final_output_w1.append([int(v) for v in top_k_pred[0][:10]])
-            final_output_w2.append([[int(v) for v in top_k_pred[i][:10]] for i in range(len(image))])
+            final_output_w2.append(
+                [[int(v) for v in top_k_pred[i][:10]] for i in range(len(image))])
 
             if not inference_only:
                 for metric in self.metrics:
@@ -155,9 +212,9 @@ class RetrievalDistCombTask(BaseTask):
             write_report(self.report_path, self.config, self.metrics)
         else:
             write_report(self.report_path, self.config)
-        
+
         with open(os.path.join(self.output_dir, "result_w1.pkl"), 'wb') as f:
             pickle.dump(final_output_w1, f)
-        
+
         with open(os.path.join(self.output_dir, "result_w2.pkl"), 'wb') as f:
             pickle.dump(final_output_w2, f)
